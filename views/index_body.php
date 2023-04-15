91fdbfde0dc53c0b44baf62d09b6e5f51b027137
Alice Nguyen <90719648+aintic@users.noreply.github.com>
2023-04-15 11:00:35 -0400

    add nav bar to every page

--- views/index_body.php
+++ views/index_body.php
@@ -1,39 +1,11 @@
-<!DOCTYPE php>
-<php>
 <head>
 	<title>My Website</title>
-
     <style>
-body {
-  background-image: linear-gradient(to bottom right, #FFA07A, #FF7F50, #FF6347, #FF4500);
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-attachment: fixed;
-}
+        body {
+        background-image: linear-gradient(to bottom right, #FFA07A, #FF7F50, #FF6347, #FF4500);
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-attachment: fixed;
+        }
     </style>
 </head>
-<body>
-	<nav>
-		<ul>
-			<li><a href="facilities.php">Facilities</a></li>
-			<li><a href="employees.php">Employees</a></li>
-			<li><a href="vaccinations.php">Vaccinations</a></li>
-			<li><a href="infections.php">Infections</a></li>
-			<li><a href="workschedules.php">Work Schedule</a></li>
-			<li><a href="6.php">6</a></li>
-            <li><a href="7.php">7</a></li>
-            <li><a href="8.php">8</a></li>
-            <li><a href="9.php">9</a></li>
-            <li><a href="10.php">10</a></li>
-            <li><a href="11.php">11</a></li>
-            <li><a href="12.php">12</a></li>
-            <li><a href="13.php">13</a></li>
-            <li><a href="14.php">14</a></li>
-            <li><a href="queries.php">15</a></li>
-            <li><a href="16.php">16</a></li>
-            <li><a href="17.php">17</a></li>
-
-		</ul>
-	</nav>
-</body>
-</php>
