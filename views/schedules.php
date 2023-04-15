571ba9e76409b30f5afa2ed21dbcbd56b0ac4e0a
Alice Nguyen <90719648+aintic@users.noreply.github.com>
2023-04-15 10:28:34 -0400

    display schedules

--- views/schedules.php
+++ views/schedules.php
@@ -1,96 +1,32 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Work Schedules</title>
-	<style>
-		table {
-		  border-collapse: collapse;
-		  width: 100%;
-		}
+<h2>Work Schedules</h2>
+<table>
+    <thead>
+        <tr>
+            <th>Employee ID</th>
+            <th>Facility ID</th>
+            <th>Start Date and Time</th>
+            <th>End Date and Time</th>
+            <th>Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        <td>
+            <button class="button create-button">Add</button>
+        </td>
 
-		th, td {
-		  text-align: left;
-		  padding: 8px;
-		  border-bottom: 1px solid #ddd;
-		}
+        <?php foreach ($records as $record): ?>
 
-		th {
-		  background-color: #4CAF50;
-		  color: white;
-		}
+        <tr class="table">
+            <td class="cell"><?= $record["EID"] ?></td>
+            <td class="cell"><?= $record["FID"] ?></td>
+            <td class="cell"><?= $record["StartDateTime"] ?></td>
+            <td class="cell"><?= $record["EndDateTime"] ?></td>
+            <td colspan="3">
+                <button class="button edit-button">Edit</button>
+                <button class="button delete-button">Delete</button>
+            </td>
+        </tr>
 
-		tr:hover {background-color: #f5f5f5;}
-
-		.button {
-		  background-color: #4CAF50;
-		  border: none;
-		  color: white;
-		  padding: 8px 16px;
-		  text-align: center;
-		  text-decoration: none;
-		  display: inline-block;
-		  font-size: 14px;
-		  margin: 4px 2px;
-		  cursor: pointer;
-		}
-
-		.create-button {
-		  background-color: #008CBA;
-		}
-
-		.edit-button {
-		  background-color: #f44336;
-		}
-
-		.delete-button {
-		  background-color: #555555;
-		}
-	</style>
-</head>
-<body>
-	<h1>Employee Schedule</h1>
-	<table>
-		<thead>
-			<tr>
-				<th>Employee Name</th>
-				<th>Schedule</th>
-				<th>Action</th>
-			</tr>
-		</thead>
-		<tbody>
-			
-			<!-- PHP code to fetch and display employee schedules -->
-			<!-- <?php
-				$sql = "SELECT e.EmployeeName, s.ScheduleDate FROM EmployeeSchedule es JOIN Employee e ON es.EmployeeID = e.EmployeeID JOIN Schedule s ON es.ScheduleID = s.ScheduleID";
-				$result = mysqli_query($conn, $sql);
-
-				if (mysqli_num_rows($result) > 0) {
-					while($row = mysqli_fetch_assoc($result)) {
-						echo "<tr>";
-						echo "<td>" . $row['EmployeeName'] . "</td>";
-						echo "<td>" . $row['ScheduleDate'] . "</td>";
-						echo "<td>";
-						echo "<button class='button edit-button' onclick=\"editEmployeeSchedule('" . $row['EmployeeName'] . "', '" . $row['ScheduleDate'] . "')\">Edit</button>";
-						echo "<button class='button delete-button' onclick=\"deleteEmployeeSchedule('" . $row['EmployeeName'] . "', '" . $row['ScheduleDate'] . "')\">Delete</button>";
-						echo "</td>";
-						echo "</tr>";
-					}
-				} else {
-					echo "<tr><td colspan='3'>No employee schedules found</td></tr>";
-				}
-
-				mysqli_close($conn);
-			?> -->
-		</tbody>
-		<tfoot>
-			<tr>
-			  <td colspan="3">
-				<button class="button create-button">Add</button>
-				<button class="button edit-button">Edit</button>
-				<button class="button delete-button">Delete</button>
-			  </td>
-			</tr>
-		  </tfoot>
-	</table>
-</body>
-</html>
+        <?php endforeach ?>
+    </tbody>
+</table>
--- views/vaccinations.php
+++ views/vaccinations.php
@@ -1,54 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Vaccine Table</title>
-	<style>
-		table {
-		  border-collapse: collapse;
-		  width: 100%;
-		}
-	  
-		th, td {
-		  text-align: left;
-		  padding: 8px;
-		  border-bottom: 1px solid #ddd;
-		}
-	  
-		th {
-		  background-color: #4CAF50;
-		  color: white;
-		}
-	  
-		tr:hover {background-color: #f5f5f5;}
-	  
-		.button {
-		  background-color: #4CAF50;
-		  border: none;
-		  color: white;
-		  padding: 8px 16px;
-		  text-align: center;
-		  text-decoration: none;
-		  display: inline-block;
-		  font-size: 14px;
-		  margin: 4px 2px;
-		  cursor: pointer;
-		}
-	  
-		.create-button {
-		  background-color: #008CBA;
-		}
-	  
-		.edit-button {
-		  background-color: #f44336;
-		}
-	  
-		.delete-button {
-		  background-color: #555555;
-		}
-	  </style>
-</head>
 <h2>Vaccinations</h2>
-<body>
 	<table>
 		<thead>
 			<tr>
@@ -57,42 +7,29 @@
 				<th>Dose Number</th>
 				<th>Date</th>
 				<th>FID</th>
+				<th>Actions</th>
 			</tr>
 		</thead>
 		<tbody>
 
+		<td>
+            <button class="button create-button">Add</button>
+        </td>
+
 		<?php foreach ($records as $record): ?>
 
 		<tr class="table">
-    	<td class="cell"><?= $record["EID"] ?></td>
-    	<td class="cell"><?= $record["VaccineType"] ?></td>
-    	<td class="cell"><?= $record["DoseNumber"] ?></td>
-		<td class="cell"><?= $record["Date"] ?></td>
-    	<td class="cell"><?= $record["FID"] ?></td>
+			<td class="cell"><?= $record["EID"] ?></td>
+			<td class="cell"><?= $record["VaccineType"] ?></td>
+			<td class="cell"><?= $record["DoseNumber"] ?></td>
+			<td class="cell"><?= $record["Date"] ?></td>
+			<td class="cell"><?= $record["FID"] ?></td>
+			<td colspan="3">
+					<button class="button edit-button">Edit</button>
+					<button class="button delete-button">Delete</button>
+			</td>
 		</tr>
 
 		<?php endforeach ?>
-
-	</tbody>
-		<tfoot>
-			<tr>
-			  <td colspan="3">
-				<button class="button create-button">Add</button>
-				<button class="button edit-button">Edit</button>
-				<button class="button delete-button">Delete</button>
-			  </td>
-			</tr>
-		  </tfoot>
-	</table>	
-			<!-- <?php
-			?> -->
-	
-	<script>
-		function deleteVaccine(vaccineType) {
-			if (confirm("Are you sure you want to delete this vaccine?")) {
-				window.location.href = "delete_vaccine.php?vaccineType=" + vaccineType;
-			}
-		}
-	</script>
-</body>
-</html>
+    </tbody>
+</table>
