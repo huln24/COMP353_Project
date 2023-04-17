b9d307cd291a85fd3feea42e4029217dd26edb8c
Alice Nguyen <90719648+aintic@users.noreply.github.com>
2023-04-17 18:45:25 -0400

    fix nav bar

--- views/header.php
+++ views/header.php
@@ -17,3 +17,17 @@
 			<li><a href="infections.php">Infections</a></li>
 			<li><a href="schedules.php">Work Schedule</a></li>
 			<li><a href="6.php">6</a></li>
+            <li><a href="7.php">7</a></li>
+            <li><a href="8.php">8</a></li>
+            <li><a href="9.php">9</a></li>
+            <li><a href="10.php">10</a></li>
+            <li><a href="11.php">11</a></li>
+            <li><a href="12.php">12</a></li>
+            <li><a href="13.php">13</a></li>
+            <li><a href="14.php">14</a></li>
+            <li><a href="15.php">15</a></li>
+            <li><a href="16.php">16</a></li>
+            <li><a href="17.php">17</a></li>
+
+		</ul>
+	</nav>
